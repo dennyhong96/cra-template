ddeb6edadbe6fa8618b5728adba0b070f45677f3
Denny Hong <hong961127@gmail.com>
2021-09-09 15:21:50 -0400

    add msw

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,4 +4,16 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
+// import { server } from "src/__tests__/mocks/server";
+
 jest.setTimeout(30 * 1000);
+
+// // Establish API mocking before all tests.
+// beforeAll(() => server.listen());
+
+// // Reset any request handlers that we may add during the tests,
+// // so they don't affect other tests.
+// afterEach(() => server.resetHandlers());
+
+// // Clean up after the tests are finished.
+// afterAll(() => server.close());
